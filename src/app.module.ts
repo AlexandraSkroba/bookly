65cf989ce2ce39cb0d52a53038c8e465ea694cdc
AlexandraSkroba <aleksa1012016@gmail.com>
2024-12-18 18:58:53 +0300

    BK-10: Configure BullMQ for Jobs enqueuing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { AuthService } from './auth/auth.service';
 import { DatabaseModule } from './database/database.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule, AuthModule],
+  imports: [ConfigModule.forRoot(),
+            DatabaseModule,
+            UserModule,
+            AuthModule,
+            BullModule.forRoot({
+              connection: {
+                host: process.env.REDIT_HOST || 'localhost',
+                port: parseInt(process.env.REDIS_PORT) || 6379
+              }
+            }),
+            BullModule.registerQueue({
+              name: 'confirmation'
+            })]
 })
 export class AppModule {}
