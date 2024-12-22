51bc67b523cd07173272da2ce43182f689fdaa35
AlexandraSkroba <AlexandraSkroba@users.noreply.github.com>
2024-12-22 08:17:50 +0300

    Quick fixex for frontend integration

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT && 3000);
+  await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
     module.hot.accept();
